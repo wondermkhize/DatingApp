f4e58f9588620ead930cde700710cd6cfd892a38
wondermkhize <wondermkhize7@gmail.com>
2020-05-21 17:17:50 +0200

    Pre-publishing

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -32,11 +32,32 @@ namespace DatingApp.API
 
         public IConfiguration Configuration { get; }
 
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+             services.AddDbContext<DataContext>(x => {
+                 x.UseLazyLoadingProxies();
+                 x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+             });
+
+            ConfigureServices(services);
+        }
+
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+             services.AddDbContext<DataContext>(x => {
+                 x.UseLazyLoadingProxies();
+                 x.UseMySql(Configuration.GetConnectionString("DefaultConnection"));
+             });
+
+            ConfigureServices(services);
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite
-            (Configuration.GetConnectionString("DefaultConnection")));
+           
             services.AddControllers().AddNewtonsoftJson(opt => 
             {
                 opt.SerializerSettings.ReferenceLoopHandling = 
@@ -93,9 +114,14 @@ namespace DatingApp.API
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
+
             });
         }
     }
